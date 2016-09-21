5cf4c822afc87b8c7003ff809c2f307ded80f96a
weilidai <weilidai2001@hotmail.com>
2016-09-21 20:16:39 +0100

    Added sorting to backend logic

--- routes/product-list.js
+++ routes/product-list.js
@@ -1,7 +1,23 @@
 var config = require('../config/config');
+var _ = require('lodash');
 
 // Mock API using fixture so we're not dependent on network connectivity
 var allProducts = require(config.ROOT +'/fixtures/products.json').data;
+
+const sortByPriceAsc = list => _.sortBy(list, item => item.price.net);
+const sortByPriceDesc = list => _.sortBy(list, item => -item.price.net);
+const sortByDesigner = list => _.sortBy(list, item => item.brand.name.en);
+
+const filterBy = (method, list) => {
+    if(method == "price_asc") {
+        return sortByPriceAsc(list)
+    } else if (method == "price_desc"){
+        return sortByPriceDesc(list)
+    } else if (method == "designer") {
+        return sortByDesigner(list)
+    }
+    return list;
+};
 
 var routes = {
     init: function(app) {
@@ -13,12 +29,15 @@ var routes = {
             if (offset > total) {
                 return res.type('json').sendStatus(400);
             }
+            var sortMethod = req.query.sortBy;
+
+            const sorted = filterBy(sortMethod, allProducts);
 
             res.json({
                 offset: offset,
                 limit: limit,
                 total: total,
-                data: allProducts.slice(offset, offset+limit).map(function(product) {
+                data: sorted.slice(offset, offset+limit).map(function(product) {
                     // Simplify payload - more data available in fixture
                     return {
                         id: product.id,
